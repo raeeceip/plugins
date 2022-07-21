3b8f547c57c42b007a86c81fb41567a03f34fda1
Chiso <chiboguchisomu@gmail.com>
2022-07-21 01:06:58 +0100

    com

--- simple-contact-form/simple-contact-form.php
+++ simple-contact-form/simple-contact-form.php
@@ -51,130 +51,33 @@ add_menu_page(
 function test_page()
 {
 ?>
-<html>
-<head>
-	<title>Slide Navbar</title>
-	<link rel="stylesheet" type="text/css" href="slide navbar style.css">
-<link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
-<style type="text/css">
-	body{
-	margin: 0;
-	padding: 0;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	min-height: 100vh;
-	font-family: 'Jost', sans-serif;
-	background: linear-gradient(to bottom, #0f0c29, #302b63, #24243e);
-}
-.main{
-	width: 350px;
-	height: 500px;
-	background: yellow;
-	overflow: hidden;
- 
-	border-radius: 10px;
-	box-shadow: 5px 20px 50px #000;
-}
-#chk{
-	display: none;
-}
-.signup{
-	position: relative;
-	width:100%;
-	height: 100%;
-}
-label{
-	color: #fff;
-	font-size: 2.3em;
-	justify-content: center;
-	display: flex;
-	margin: 60px;
-	font-weight: bold;
-	cursor: pointer;
-	transition: .5s ease-in-out;
-}
-input{
-	width: 60%;
-	height: 20px;
-	background: #e0dede;
-	justify-content: center;
-	display: flex;
-	margin: 20px auto;
-	padding: 10px;
-	border: none;
-	outline: none;
-	border-radius: 5px;
-}
-button{
-	width: 60%;
-	height: 40px;
-	margin: 10px auto;
-	justify-content: center;
-	display: block;
-	color: #fff;
-	background: #573b8a;
-	font-size: 1em;
-	font-weight: bold;
-	margin-top: 20px;
-	outline: none;
-	border: none;
-	border-radius: 5px;
-	transition: .2s ease-in;
-	cursor: pointer;
-}
-button:hover{
-	background: #6d44b8;
-}
-.login{
-	height: 460px;
-	background: #eee;
-	border-radius: 60% / 10%;
-	transform: translateY(-180px);
-	transition: .8s ease-in-out;
-}
-.login label{
-	color: #573b8a;
-	transform: scale(.6);
-}
+<form action="menu_page.php" style="border:1px solid #ccc id="simple_contact_form__form"">
+  <div class="container">
+    <h1>Sign Up</h1>
+    <p>Please fill in this form to create an account.</p>
+    <hr>
 
-#chk:checked ~ .login{
-	transform: translateY(-500px);
-}
-#chk:checked ~ .login label{
-	transform: scale(1);	
-}
-#chk:checked ~ .signup label{
-	transform: scale(.6);
-}
+    <label for="email"><b>Email</b></label>
+    <input type="text" placeholder="Enter Email" name="email" required>
 
-</style>
-</head>
-<body>
-	<div class="main">  	
-		<input type="checkbox" id="chk" aria-hidden="true">
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
 
-			<div class="signup">
-				<form>
-					<label for="chk" aria-hidden="true">Wecome To Perzi</label>
-					<input type="text" name="txt" placeholder="User name" required="">
-					<input type="email" name="email" placeholder="Email" required="">
-					<input type="password" name="pswd" placeholder="Password" required="">
-					<button>Sign up</button>
-				</form>
-			</div>
+    <label for="psw-repeat"><b>Repeat Password</b></label>
+    <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
 
-			<div class="login">
-				<form>
-					<label for="chk" aria-hidden="true">Login</label>
-					<input type="email" name="email" placeholder="Email" required="">
-					<input type="password" name="pswd" placeholder="Password" required="">
-					<button>Login</button>
-				</form>
-			</div>
-	</div>
-</body>
-</html>
+    <label>
+      <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
+    </label>
+
+    <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
+
+    <div class="clearfix">
+      <button type="button" class="cancelbtn">Cancel</button>
+      <button type="submit" class="signupbtn">Sign Up</button>
+    </div>
+  </div>
+</form>
 <?php
 
 
@@ -187,7 +90,7 @@ class SimpleContactForm{
 
     add_action('wp_enqueue_scripts', array($this, 'load_assets'));
 
-    add_shortcode('contact_form', array($this, 'load_shortcode'));
+    add_shortcode('contact-form', array($this, 'load_shortcode'));
 
     
   }
@@ -225,20 +128,29 @@ class SimpleContactForm{
   }
 
   public function load_shortcode(){
-      return '
-    <div class="col-lg-6 col-6 pd-l">
-      [text* your-name placeholder"Name"]
-      [text* your-sub placeholder"subject"]
-    </div>
-    <div class="col-lg-6 col-12 pd-r">
-      [email* your-email placeholder"Email"]
-      [tel* tel-672 your-phone placeholder"Phone"]
-    </div>
-    <div class="col-12">
-      [textarea your-message placeholder"message"]
-    </div>
-    <div class="col-12 btn-sub">[submit "Send"]</div>';
-  }
+    ?>
+      <div class="simple-contact-form" >
+          <h1>Contact us</h1>
+          <form id="simple-contact-form__form ">
+          <div class="form-group mb-2">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            
+          </div>
+
+          <div class="form-group mb-2">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+          </div>
+          <button type="submit" class="btn btn-success btn-block w-100">Submit</button>
+        </form>
+      
+      </div>
+      <?php
+    
+
+  
+   }
 
 }
 

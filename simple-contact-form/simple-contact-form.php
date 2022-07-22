88b19c1aabb6b83b42d90a3cfb9d011f812956af
Chiso <chiboguchisomu@gmail.com>
2022-07-22 02:39:27 +0100

    committing

--- simple-contact-form/simple-contact-form.php
+++ simple-contact-form/simple-contact-form.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Plugin Name:        Simple Contact Form
- * Description:       Extends the WP REST API for Developing a Simple Contact Form.
+ * Plugin Name:       Perzi
+ * Description:       Uses REST API to extend functionality of perzi platform onto Wordpress.
  * Version:           1.0.0
  * Author:            Chibogu Chisom
  * Text Domain:       simple-contact-form
@@ -22,8 +22,7 @@ add_action('admin_menu', 'function_create_menu');
 // Create WordPress admin menu
 function function_create_menu(){
 
-//The icon in Base64 format
-$icon_base64 = 'PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9Im5vIj8+PCFET0NUWVBFIHN2ZyBQVUJMSUMgIi0vL1czQy8vRFREIFNWRyAxLjEvL0VOIiAiaHR0cDovL3d3dy53My5vcmcvR3JhcGhpY3MvU1ZHLzEuMS9EVEQvc3ZnMTEuZHRkIj48c3ZnIHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIHZpZXdCb3g9IjAgMCAyMTM0IDIxMzQiIHZlcnNpb249IjEuMSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayIgeG1sOnNwYWNlPSJwcmVzZXJ2ZSIgeG1sbnM6c2VyaWY9Imh0dHA6Ly93d3cuc2VyaWYuY29tLyIgc3R5bGU9ImZpbGwtcnVsZTpldmVub2RkO2NsaXAtcnVsZTpldmVub2RkO3N0cm9rZS1saW5lY2FwOnJvdW5kO3N0cm9rZS1saW5lam9pbjpyb3VuZDtzdHJva2UtbWl0ZXJsaW1pdDoxLjU7Ij48Zz48cmVjdCB4PSIyODIuNTc2IiB5PSI4OTAuODg2IiB3aWR0aD0iNzc1Ljc1OCIgaGVpZ2h0PSI3NzguNzgxIiBzdHlsZT0iZmlsbDojZmZmO2ZpbGwtb3BhY2l0eTowO3N0cm9rZTojMDAxZGZmO3N0cm9rZS13aWR0aDo2Ni42N3B4OyIvPjxyZWN0IHg9IjEwNzMuODkiIHk9Ijg5MC44ODYiIHdpZHRoPSI3NzUuNzU4IiBoZWlnaHQ9Ijc3OC43ODEiIHN0eWxlPSJmaWxsOiNmZmY7ZmlsbC1vcGFjaXR5OjA7c3Ryb2tlOiMwMDFkZmY7c3Ryb2tlLXdpZHRoOjY2LjY3cHg7Ii8+PHJlY3QgeD0iNjc4LjIzMiIgeT0iMTA2LjUzNyIgd2lkdGg9Ijc3NS43NTgiIGhlaWdodD0iNzc4Ljc4MSIgc3R5bGU9ImZpbGw6I2ZmZjtmaWxsLW9wYWNpdHk6MDtzdHJva2U6IzAwMWRmZjtzdHJva2Utd2lkdGg6NjYuNjdweDsiLz48cGF0aCBkPSJNMTE3My43MywzMy4zNjZsLTIxNS4yOTEsLTBsLTAsMzY3LjI0MWwxMDcuMTMxLC0xMTkuMTA2bDEwOC4xNiwxMTkuMTA2bC0wLC0zNjcuMjQxWiIgc3R5bGU9ImZpbGw6I2ZmZjtmaWxsLW9wYWNpdHk6MDtzdHJva2U6IzAwMWRmZjtzdHJva2Utd2lkdGg6NjYuNjdweDsiLz48cGF0aCBkPSJNNzgxLjg0OCw4OTQuMDc1bC0yMTUuMjkxLDBsLTAsMzUzLjc3MWwxMDcuMTMxLC0xMTQuNzM3bDEwOC4xNiwxMTQuNzM3bC0wLC0zNTMuNzcxWiIgc3R5bGU9ImZpbGw6I2ZmZjtmaWxsLW9wYWNpdHk6MDtzdHJva2U6IzAwMWRmZjtzdHJva2Utd2lkdGg6NjYuNjdweDsiLz48cGF0aCBkPSJNMTU2NC45OCw4OTUuNDc0bC0yMTUuMjkxLC0wbC0wLDM1My43NzFsMTA3LjEzLC0xMTQuNzM3bDEwOC4xNjEsMTE0LjczN2wtMCwtMzUzLjc3MVoiIHN0eWxlPSJmaWxsOiNmZmY7ZmlsbC1vcGFjaXR5OjA7c3Ryb2tlOiMwMDFkZmY7c3Ryb2tlLXdpZHRoOjY2LjY3cHg7Ii8+PHJlY3QgeD0iOTk2LjMwMiIgeT0iNTAxLjQ5NyIgd2lkdGg9IjEzOS41NjEiIGhlaWdodD0iNjQuMjMxIiBzdHlsZT0iZmlsbDojMDAxZGZmO3N0cm9rZTojMDAxZGZmO3N0cm9rZS13aWR0aDoxMS42M3B4OyIvPjxyZWN0IHg9IjYwNC40MjIiIHk9IjEzNTUuODkiIHdpZHRoPSIxMzkuNTYxIiBoZWlnaHQ9IjY0LjIzMSIgc3R5bGU9ImZpbGw6IzAwMWRmZjtzdHJva2U6IzAwMWRmZjtzdHJva2Utd2lkdGg6MTEuNjNweDsiLz48cmVjdCB4PSIxMzg3LjU1IiB5PSIxMzU1Ljg5IiB3aWR0aD0iMTM5LjU2MSIgaGVpZ2h0PSI2NC4yMzEiIHN0eWxlPSJmaWxsOiMwMDFkZmY7c3Ryb2tlOiMwMDFkZmY7c3Ryb2tlLXdpZHRoOjExLjYzcHg7Ii8+PHJlY3QgeD0iMTQxLjAzOCIgeT0iMTY3MS4yOCIgd2lkdGg9IjE4NDcuMDMiIGhlaWdodD0iMTQyLjg0IiBzdHlsZT0iZmlsbDpub25lO3N0cm9rZTojMDAxZGZmO3N0cm9rZS13aWR0aDo2Ni42N3B4OyIvPjxyZWN0IHg9IjE0MC4wMDMiIHk9IjE5NTUuNDMiIHdpZHRoPSIxODQ5LjEiIGhlaWdodD0iMTQyLjg0IiBzdHlsZT0iZmlsbDpub25lO3N0cm9rZTojMDAxZGZmO3N0cm9rZS13aWR0aDo2Ni42N3B4OyIvPjxyZWN0IHg9IjIxMy4yMzYiIHk9IjE4MTMuNyIgd2lkdGg9IjE0Mi40MTIiIGhlaWdodD0iMTQxLjQ3OSIgc3R5bGU9ImZpbGw6bm9uZTtzdHJva2U6IzAwMWRmZjtzdHJva2Utd2lkdGg6NjYuNjdweDsiLz48cmVjdCB4PSI5OTUuOTIxIiB5PSIxODEzLjciIHdpZHRoPSIxNDIuNDEyIiBoZWlnaHQ9IjE0MS40NzkiIHN0eWxlPSJmaWxsOm5vbmU7c3Ryb2tlOiMwMDFkZmY7c3Ryb2tlLXdpZHRoOjY2LjY3cHg7Ii8+PHJlY3QgeD0iMTc3OC42MSIgeT0iMTgxMy43IiB3aWR0aD0iMTQyLjQxMiIgaGVpZ2h0PSIxNDEuNDc5IiBzdHlsZT0iZmlsbDpub25lO3N0cm9rZTojMDAxZGZmO3N0cm9rZS13aWR0aDo2Ni42N3B4OyIvPjwvZz48L3N2Zz4=';
+
 
 //The icon in the data URI scheme
 $icon_data_uri = 'dashicons-products' ;
@@ -51,31 +50,41 @@ add_menu_page(
 function test_page()
 {
 ?>
-<form action="menu_page.php" style="border:1px solid #ccc id="simple_contact_form__form"">
-  <div class="container">
-    <h1>Sign Up</h1>
-    <p>Please fill in this form to create an account.</p>
+<form action="menu_page.php" style=" background-color: white; display: inline-block;padding: 10px; border-radius: 10px; margin: 10px 10px 10% 35%; align-self: middle; text-align: center;color: grey; width: 420px;" #ccc id="simple_contact_form__form">
+  <div class="container" style="justify-content: center; width: 100%; ">
+    <h1>Perzi</h1>
+    <p>Login/Signup</p>
     <hr>
+    <div >
+      <label for="email"><b>Email</b></label>
+      <br/>
+      <input type="text" placeholder="Enter Email" name="email" style="width:100%;" required>
+    </div>
 
-    <label for="email"><b>Email</b></label>
-    <input type="text" placeholder="Enter Email" name="email" required>
+    <div>
+      <label for="psw"><b>Password</b></label>
+      <br/>
+      <input type="password" placeholder="Enter Password" name="psw"  style="width:100%;" required>
+    </div>
 
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
-
-    <label for="psw-repeat"><b>Repeat Password</b></label>
-    <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
-
+    <div>
+        <label for="psw-repeat"><b>Repeat Password</b></label>
+        <br/>
+        <input type="password" placeholder="Repeat Password" name="psw-repeat" style="width:100%; box-shadow: 4px 4px 5px 5px #88888;" required>
+    </div>
+      <br/>
+    <div>
     <label>
-      <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
+      <input type="checkbox" checked="checked" name="remember" > Remember me
     </label>
+    </div>
+
 
     <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
 
-    <div class="clearfix">
-      <button type="button" class="cancelbtn">Cancel</button>
-      <button type="submit" class="signupbtn">Sign Up</button>
-    </div>
+    <div class="clearfix" style="width=100%;">
+      <button type="submit" class="signupbtn" style="padding:10px; border:1px solid gray; border-radius: 5px; background-color: yellow; width: 80%;">Sign Up</button>
+</div>
   </div>
 </form>
 <?php
@@ -86,11 +95,20 @@ function test_page()
 class SimpleContactForm{
   public function __construct()
   {
-    add_action('init', array($this, 'create_custom_post_type'));
+    add_action( 'init', array($this, 'create_posttype' ));
 
+    add_action('init', array($this, 'create_custom_post_type'));
+      /*
     add_action('wp_enqueue_scripts', array($this, 'load_assets'));
+    */
 
     add_shortcode('contact-form', array($this, 'load_shortcode'));
+
+    add_action('wp_footer', array($this, 'load_scripts'));
+
+    add_action('rest_api_init', array($this, 'register_rest_api'));
+
+   
 
     
   }
@@ -113,43 +131,122 @@ class SimpleContactForm{
         );
         register_post_type('simple_contact_form', $args);
   }
+  public function create_posttype() {
+ 
+    register_post_type( 'Products',
+    // CPT Options
+        array(
+            'labels' => array(
+                'name' => __( 'Products' ),
+                'singular_name' => __( 'Product' )
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array('slug' => 'Products'),
+            'show_in_rest' => true,
+ 
+        )
+    );
+}
+  
   public function load_assets(){
+    /*
     wp_enqueue_style(
         'simple-contact-form-style', 
         plugin_dir_url(__FILE__).'CSS/simple-contact-form.css',
         array(),
         '1.0.0',
         'all');
+    */
     wp_enqueue_script('simple-contact-form-script',
         plugin_dir_url(__FILE__).'JS/simple-contact-form.js',
-        array('jquery'),
+        array('jQuery'),
         '1.0.0',
         true);
   }
+  
 
   public function load_shortcode(){
-    ?>
-      <div class="simple-contact-form" >
-          <h1>Contact us</h1>
-          <form id="simple-contact-form__form ">
-          <div class="form-group mb-2">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            
-          </div>
+    return '
 
-          <div class="form-group mb-2">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
-          </div>
-          <button type="submit" class="btn btn-success btn-block w-100">Submit</button>
-        </form>
+  <div class="simple-contact-form" style=" background-color: blue; display: inline-block;padding: 10px; border-radius: 10px; margin: 10px 10px 10% 35%; align-self: middle; text-align: center;color: grey; width: 420px;" #ccc id="simple_contact_form__form" >
+    <h1>Contact us</h1>
+    <form id="simple-contact-form__form ">
+    <div class="form-group mb-3">
+      <label for="exampleInputEmail1" class="form-label">Email address</label>
+      <br/>
+      <input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp">
       
-      </div>
-      <?php
-    
+    </div>
 
-  
+    <div class="form-group mb-3">
+      <label for="exampleInputPassword1" class="form-label">Password</label>
+      <br/>
+      <input type="text-area" class="form-control" id="InputPassword1">
+    </div>
+    <button type="submit" class="btn btn-success btn-block w-100">Submit</button>
+  </form>
+
+  </div>
+    
+    '
+
+      
+
+    ;
+
+    
+   }
+   public function load_scripts()
+   {?>
+      <script src="http://code.jquery.com/jquery-1.11.3.min.js">
+        (function($){
+          $(document).ready(function(){
+            $('#simple-contact-form__form').on('submit', function(e){
+              e.preventDefault();
+              var form = $(this);
+              var formData = $(this).serialize();
+              $.ajax({
+                url: '<?php echo get_rest_url(null, null); ?>',
+                method: 'POST',
+                data: formData,
+                headers: {
+                  'X-WP-Nonce': '<?php echo wp_create_nonce('wp_rest'); ?>'
+                },
+                success: function(data){
+                  console.log(data);
+                  form.trigger('reset');
+                }
+              });
+            });
+          });
+        })(jQuery);
+      </script>
+
+   <?php
+   }
+   public function register_rest_routes()
+   {
+    register_rest_route('simple-contact-form/v1', 'send-email', array(
+      'methods' => 'POST',
+      'callback' => array($this, 'handle_contac_form')
+    ));
+   }
+   public function handle_contac_form($request)
+   {
+    $email = $request->get_param('email');
+    $message = $request->get_param('message');
+    $name = $request->get_param('name');
+    $subject = $request->get_param('subject');
+    $to = '
+    <div>
+      <h1>'.$name.'</h1>
+      <p>'.$message.'</p>
+    </div>
+    ';
+    $headers = array('Content-Type: text/html; charset=UTF-8');
+    wp_mail($email, $subject, $to, $headers);
+    return new WP_REST_Response(array('message' => 'Email sent successfully'), 200);
    }
 
 }
